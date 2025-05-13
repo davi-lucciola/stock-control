5eee1ff76026208f640ddde95296f3b8bbcaa793
Davi Lucciola <davilucciola14@gmail.com>
2025-05-12 23:00:36 -0300

    [feature:backend/poetry-to-uv] - changing from poetry to uv

--- server/api/db.py
+++ server/api/db.py
@@ -1,9 +1,12 @@
-import decouple as env
+import os
+import dotenv as env
 from typing import Generator
 from sqlmodel import Session, create_engine
 
 
-engine = create_engine(env.config('DATABASE_URL'))
+env.load_dotenv()
+engine = create_engine(os.getenv('DATABASE_URL'))
+
 
 def get_db() -> Generator[Session, None, None]:
     global engine
